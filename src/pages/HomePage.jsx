8b9111a93100029c1931d45982c14b66a6fc8626
KinAxell <kin@axell.tokyo>
2023-02-12 22:21:53 +0900

    add BrowseRoom and JustArrived components and add images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
+import BrowseRoom from "../parts/HomePage/BrowseRoom";
+import JustArrived from "../parts/HomePage/JustArrived";
 
 export default function HomePage() {
     return (
         <div>
             <Header />
             <Hero />
+            <BrowseRoom />
+            <JustArrived />
         </div>
     );
 }
